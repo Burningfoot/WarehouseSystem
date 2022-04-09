e5adb441f78b07b1a41d9ea3c1042022074103f2
Burningfoot <bufoot@gmail.com>
2022-04-09 16:51:46 +0200

    Added Back Button to Mobile View

--- ViewModels/Mobile/MainViewModel.cs
+++ ViewModels/Mobile/MainViewModel.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using WarehouseSystem.Models.Tools;
 
 namespace WarehouseSystem.ViewModels.Mobile
 {
     public class MainViewModel: BaseViewModel
     {
+        public RelayCommand<Button> BackBtn { get; set; }
 
+        public MainViewModel()
+        {
+            RelayCommands();
+        }
+
+        private void RelayCommands()
+        {
+            BackBtn = new RelayCommand<Button>((o) => 
+            { 
+                ViewModels.MainViewModel.OnChangeView(new ViewModel_StartScreen());
+            });
+        }
     }
 }
